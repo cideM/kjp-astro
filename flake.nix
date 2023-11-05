ed245098bb6483619acab55e1c381da77e5c48bf
Florian Beeres <yuuki@protonmail.com>
2023-11-05 17:59:05 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "The latest NodeJS and NPM from Nixpkgs";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            coreutils
+            moreutils
+            jq
+            alejandra
+
+            # You can leverage Typescript even in a pure Javascript project.
+            pkgs.nodePackages.typescript-language-server
+            pkgs.nodePackages."@astrojs/language-server"
+            pkgs.nodePackages.vscode-langservers-extracted
+            pkgs.nodejs
+          ];
+        };
+      }
+    );
+}
