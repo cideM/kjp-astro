929d91db27a6713daece9c9db2759956780d92f5
Florian Beeres <yuuki@protonmail.com>
2024-09-08 19:25:20 +0200

    Add new photos for carousel and Praxis page

--- src/content/config.ts
+++ src/content/config.ts
@@ -17,30 +17,13 @@ const gallerie = defineCollection({
   schema: ({ image }) =>
     z.object({
       srcLandscape: image().refine(
-        (img) => Math.round(img.width / img.height) === 3,
+        (img) => Math.round(img.width / img.height) === 2,
         (img) => ({
-          message: `Landscape images must have a 3:1 aspect ratio but was ${
-            img.width / img.height
-          }}`,
-        }),
-      ),
-      srcPortrait: image().refine(
-        (img) => Math.round(img.width / img.height) === Math.round(4 / 5),
-        (img) => ({
-          message: `Landscape images must have a 4:5 aspect ratio but was ${Math.round(
+          message: `Landscape images must have a 16:9 aspect ratio but was ${Math.round(
             img.width / img.height,
-          )}`,
+          )}}`,
         }),
       ),
-      alt: z.string(),
-    }),
-});
-
-const praxis = defineCollection({
-  type: "data",
-  schema: ({ image }) =>
-    z.object({
-      src: image(),
       alt: z.string(),
     }),
 });
@@ -48,5 +31,4 @@ const praxis = defineCollection({
 export const collections = {
   therapeuten: therapeuten,
   gallerie: gallerie,
-  praxis: praxis,
 };
